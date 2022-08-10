744c5f5db9c08eae1af095072ca0fce130797b0a
P SWANEPOEL <phillip.swanepoel175@gmail.com>
2022-08-10 19:30:08 +0200

    initial commit

---
+++ New Text Document.py
@@ -0,0 +1,3 @@
+#I have no idea what code changed my perspective but the ease of use and versatility of the python language sure had a major impact on the way I see development.
+
+print("Whoop")
--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # CMPG323-Homework
 Homework exercises for CMPG323 due 11 August
+
+## Homework Exercise 1 - 11 August
+	Creating first repository - includes simple .py file.
